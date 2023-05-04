392274e104503ccd1455a5b775e4b8b069969895
Kostiantyn Honcharov <68106610+KostiantynFirst@users.noreply.github.com>
2023-05-04 20:59:22 +0300

    Update styles

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -1,26 +1,25 @@
 import styled from "styled-components";
 
 export const FilterContainer = styled.div`
-  display: flex;
-  align-items: center;
   margin-bottom: 20px;
 `;
 
 export const FilterLabel = styled.label`
-  font-size: 16px;
-  font-weight: 700;
+  font-size: 18px;
+  font-weight: bold;
   margin-right: 10px;
 `;
 
 export const FilterInput = styled.input`
-  padding: 8px 16px;
   font-size: 16px;
-  border: none;
-  border-radius: 4px;
-  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
-  outline: none;
+  padding: 5px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  width: 300px;
+`;
 
-  &:focus {
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
-  }
+export const FilterBtn = styled.button`
+  font-size: 18px;
+  padding: 10px 20px;
+  background-color: #008
 `;
--- src/components/Phonebook/Phonebook.styled.jsx
+++ src/components/Phonebook/Phonebook.styled.jsx
@@ -1,95 +1,102 @@
 import styled from "styled-components";
 
 export const PhonebookContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 50px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+`;
+
 export const PhonebookHeadings = styled.h2`
-    font-family: Arial, sans-serif;
-    margin: 0;
-    margin-bottom: 10px;
-    padding: 0;
-`
+  font-family: Arial, sans-serif;
+  margin: 0;
+  margin-bottom: 20px;
+  padding: 0;
+`;
+
 export const PhonebookForm = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 20px;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
 export const PhonebookFormContainer = styled.div`
-    display: flex;
-    alighn-items: center;
-    margin-bottom: 10px;
-`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
 
 export const PhonebookFormLabel = styled.label`
-    font-size: 18px;
-    font-weight: bold;
-    padding: 5px;
-`
+  font-size: 18px;
+  font-weight: bold;
+  margin-right: 10px;
+  padding: 5px;
+  width: 100px;
+`;
+
 export const PhonebookFormInput = styled.input`
-    font-size: 16px;
-    padding: 5px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    width: 300px;
+  font-size: 16px;
+  padding: 5px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  width: 300px;
+`;
 
-`
 export const PhonebookBtn = styled.button`
-    font-size: 18px;
-    padding: 10px 20px;
-    background-color: #008CBA;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+  font-size: 18px;
+  padding: 10px 20px;
+  background-color: #008cba;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
 
-    &:hover {
-        background-color: #006b8f;
-        }
-`
-  
+  &:hover {
+    background-color: #006b8f;
+  }
+`;
+
 export const PhonebookContacts = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const PhonebookContactsHeading = styled.h3`
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    margin-bottom: 10px;
-`
+  font-family: Arial, sans-serif;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 20px;
+`;
 
 export const PhonebookContactsList = styled.ul`
-    list-style: none;
-    margin: 0;
-    padding: 0;
-  }
-`
-export const PhonebookContactsListItem = styled.li`
-    display: flex;
-    align-items: center;
-    margin-bottom: 10px;
-`
-export const PhonebookContactsListItemName = styled.span`
-    font-size: 16px;
-    margin-right: 10px;
-`
-export const DeleteBtn = styled.button`
-    font-size: 14px;
-    padding: 5px 10px;
-    background-color: #e74c3c;
-    color: #fff;
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
 
-    &:hover {
+export const PhonebookContactsListItem = styled.li`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+`;
+
+export const PhonebookContactsListItemName = styled.span`
+  font-size: 16px;
+  margin-right: 10px;
+`;
+
+export const DeleteBtn = styled.button`
+  font-size: 14px;
+  padding: 5px 10px;
+  background-color: #e74c3c;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
     background-color: #c0392b;
   }
-`
-  
+`;
